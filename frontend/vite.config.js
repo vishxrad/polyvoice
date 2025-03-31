d1f8c7f5a4928b2a53eb7e16b748b49e90d7ce14
Nandishwar <nandishwarjasrotia@gmail.com>
2025-03-31 08:08:48 +0530

    UI2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-});
-=======
   server: {
-    allowedHosts: ['hackathon.polyvoice.tech',
-      'polyvoice.tech'
-    ]
-
-  }
-})
->>>>>>> d8b71d09c2e0eac6b627103f441b7b3ad55c3924
+    allowedHosts: ["hackathon.polyvoice.tech", "polyvoice.tech"],
+  },
+});
